253c946b7e70b280c098f39bdbd0faeb5622ef30
Wojtysek <vojtahr1@post.cz>
2021-12-13 13:55:52 +0100

    Add project files.

---
+++ nasobilka/nasobilka.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+	for (int j = 1; j <= 10; j++) {
+		//jeden radek
+		for (int i = 1; i <= 10; i++) {
+			printf("", i * j);
+		}
+		printf("\n");
+	}
+
+	//sikma plocha
+	for (int j = 1; j <= 10; j++) {
+		//i je pro 1 radek
+		for (int i = 1; i <= j; i++) {
+			printf("%3d ", i);
+		}
+		printf("\n");
+	}
+}
